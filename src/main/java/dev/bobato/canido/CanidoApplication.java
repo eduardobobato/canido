ce89bbfecffd39a56aa7cb67c9a21358f40f577f
Eduardo Bobato <eduardobobato@hotmailcom.br>
2022-03-07 19:21:07 -0300

    init

---
+++ src/main/java/dev/bobato/canido/CanidoApplication.java
@@ -0,0 +1,13 @@
+package dev.bobato.canido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bobato/canido/CanidoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bobato.canido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
